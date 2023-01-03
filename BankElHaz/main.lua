61d336404dd81bf8f4a651947e97d4a423c24ee2
Abdo Tolba <mtolba2004@gmail.com>
2023-01-03 02:19:09 +0200

    DONE!
    The game is fullly functional almost!
    main function to call for all players and editplayers
    
    editPlayers function that takes player and index, the index edits the player array objects, it simulates switch case using another object of functions and calling the function using obj['key'](param)
    
    + some cleaning to the code

--- BankElHaz/main.lua
+++ BankElHaz/main.lua
@@ -1,3 +1,54 @@
+
+function allPlayers(scores)
+
+	for i = 1 , numOfPlayers , 1
+	do
+		print(i.."- "..player[i]["name"].." "..((scores == 1 and player[i]["score"] or "")))		
+	end
+			
+end
+
+
+function main()
+	print("Enter the index  of player you want to edit : ")
+
+	allPlayers(1)
+
+	local success,err = pcall(editPlayer(player,io.read("*n","*l")))
+	if not success then 
+	error("The only acceptable inputs are (+, *, - or /)")
+	end
+end
+
+
+
+function editPlayer(player,index)
+	print("which operation do you want to do? (+, *, - or /) or type \"ex\" to exit editing mode")
+	opr = io.read("*l")
+
+
+	action = {
+		['+'] = function () print("How much to add? : ") add = io.read("*l")  player[index]["score"] = player[index]["score"] + add end, 
+		['-'] = function () print("How much to substract? : ") sub =  io.read("*l")  player[index]["score"] = player[index]["score"] - sub end, 
+ 		['/'] = function () print("How much to divaide? : ") div = io.read("*l")  player[index]["score"] = player[index]["score"] / div end,
+		['*'] = function () print("How much to multiple? : ") mul  =  io.read("*l")  player[index]["score"] = player[index]["score"] * mul end,
+	["ex"] = function () end 
+		 
+	}
+
+
+
+	if (opr == '*'or opr == '/' or opr == '-' or opr == '+' or opr == "ex") then
+		action[opr]()
+	end
+main()
+	
+end
+
+
+
+
+
 print("how many players are coming?")
 numOfPlayers = io.read("*n","*l")
 
@@ -18,21 +69,7 @@ do
 	player[i]["score"] = io.read("*n","*l")
 end
 
-function allPlayers(scores)
-	for i = 1 , numOfPlayers , 1
-	do
-		print(i.."- "..player[i]["name"].." "..((scores == 1 and player[i]["score"] or "")))		
-	end
-			
-end
-
 print("shall the Game START !!!")
-print("Enter the index  of player you want to edit : ")
 
-allPlayers(1)
-
-
-
-
-
+main()
 
